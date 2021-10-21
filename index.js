d37a59a0490d54813796064e1b346675d83dc5db
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2021-10-21 12:32:13 +0600

    my first ever node project

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+const users = [
+    {
+        "id": 0,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 1,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    }
+]
+
+app.get('/users', (req, res) => {
+    //use query parameter
+    const search = req.query.search;
+
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+//post method
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log(req.body);
+    // res.send(JSON.stringify(newUser));
+    res.json(newUser);
+});
+
+//dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+    res.send(user);
+});
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+});
